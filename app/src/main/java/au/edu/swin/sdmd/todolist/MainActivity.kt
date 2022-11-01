cd9c8e7bc3d4e321b088c9a48434e45d43060bd7
Mohammad Chohan <mohammadchohan6f@gmail.com>
2022-11-02 02:19:05 +1100

    made view id unique and refactored

--- app/src/main/java/au/edu/swin/sdmd/todolist/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/todolist/MainActivity.kt
@@ -12,8 +12,6 @@ import au.edu.swin.sdmd.todolist.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import java.time.ZonedDateTime
 
-private const val DATABASE_NAME = "TO_DO_DATABASE"
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,15 +23,7 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val data = result.data?.getParcelableExtra<ToDo>(EXTRA_UPDATED_TO_DO)!!
-
-                val dataDb = toDoRepository.loadById(data.id)
-                dataDb.apply {
-                    title = data.title
-                    reminderDateTime = data.reminderDateTime
-                }
-
-                toDoRepository.updateToDo(dataDb)
-
+                toDoRepository.updateToDo(data)
                 toDoList.clear()
                 toDoList.addAll(toDoRepository.loadAll())
                 toDoAdapter.notifyDataSetChanged()
@@ -49,15 +39,14 @@ class MainActivity : AppCompatActivity() {
         toDoList.addAll(toDoRepository.loadAll())
 
         toDoAdapter = ToDoAdapter(toDoList, detailLauncher)
-        binding.toDoList.apply {
+        binding.toDoRecycler.apply {
             adapter = toDoAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
 
         binding.floatingActionButton.setOnClickListener {
             val newToDo = ToDo(
-                title = "To Do",
-                reminderDateTime = ZonedDateTime.now()
+                title = "To Do", reminderDateTime = ZonedDateTime.now()
             )
             val newId = toDoRepository.insert(newToDo)
             toDoList.clear()
@@ -67,7 +56,6 @@ class MainActivity : AppCompatActivity() {
                 DetailActivity.EXTRA_TO_DO, toDoRepository.loadById(newId)
             )
             detailLauncher.launch(intent)
-
         }
 
         ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
@@ -80,20 +68,20 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val deletedToDo: ToDo = toDoList[viewHolder.adapterPosition]
+                val position = viewHolder.adapterPosition
+                val deletedToDo: ToDo = toDoList[position]
                 toDoRepository.delete(deletedToDo)
                 toDoList.clear()
                 toDoList.addAll(toDoRepository.loadAll())
-                val position = viewHolder.adapterPosition
                 toDoAdapter.notifyItemRemoved(position)
                 Snackbar.make(
-                    binding.toDoList, "Deleted " + deletedToDo.title, Snackbar.LENGTH_LONG
+                    binding.toDoRecycler, "Deleted " + deletedToDo.title, Snackbar.LENGTH_LONG
                 ).setAction("Undo") {
                     toDoRepository.insert(deletedToDo)
                     toDoAdapter.toDoList.add(position, deletedToDo)
                     toDoAdapter.notifyItemInserted(position)
                 }.show()
             }
-        }).attachToRecyclerView(binding.toDoList)
+        }).attachToRecyclerView(binding.toDoRecycler)
     }
 }
