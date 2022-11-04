a44e632f4dfded8267afaf033f35b0c2cca8476d
Mohammad Chohan <mohammadchohan6f@gmail.com>
2022-11-05 03:57:45 +1100

    fix itemId adapter issue

--- app/src/main/java/au/edu/swin/sdmd/todolist/ToDoAdapter.kt
+++ app/src/main/java/au/edu/swin/sdmd/todolist/ToDoAdapter.kt
@@ -1,18 +1,13 @@
 package au.edu.swin.sdmd.todolist
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
 import androidx.recyclerview.widget.RecyclerView
 import au.edu.swin.sdmd.todolist.databinding.ToDoItemBinding
 import java.time.format.DateTimeFormatter
 
-const val EXTRA_UPDATED_TO_DO = "EXTRA_UPDATED_TO_DO"
-
 class ToDoAdapter(
-    val toDoList: List<ToDo>,
-    private val onToDoClicked: (toDoId: Long) -> Unit
+    val toDoList: List<ToDo>, private val onToDoClicked: (toDoId: Long) -> Unit
 ) : RecyclerView.Adapter<ToDoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
@@ -32,14 +27,18 @@ class ToDoAdapter(
         return toDoList[position].id
     }
 
+    init {
+        setHasStableIds(true)
+    }
+
     companion object {
         val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
         val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM")
     }
 }
 
-class ToDoViewHolder(private val binding: ToDoItemBinding) :
-    RecyclerView.ViewHolder(binding.root) {
+class ToDoViewHolder(private val binding: ToDoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
     fun bind(toDo: ToDo, onToDoClicked: (toDoId: Long) -> Unit) {
         binding.toDoItemTitle.text = toDo.title
         binding.reminderDate.text = toDo.reminderDateTime.format(ToDoAdapter.dateFormatter)
