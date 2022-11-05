fddf6efd815885a3cd0d4686f7866d92dff49fbd
Mohammad Chohan <mohammadchohan6f@gmail.com>
2022-11-05 13:28:19 +1100

    remove parcelable

--- app/src/main/java/au/edu/swin/sdmd/todolist/ToDo.kt
+++ app/src/main/java/au/edu/swin/sdmd/todolist/ToDo.kt
@@ -1,15 +1,12 @@
 package au.edu.swin.sdmd.todolist
 
-import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 import java.time.ZonedDateTime
 
-@Parcelize
 @Entity
 data class ToDo(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val title: String,
     val reminderDateTime: ZonedDateTime,
-) : Parcelable
+)
